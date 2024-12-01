679c29adb13138b30ea7b7bc1c7277838ac840b3
CamelotMacaw <91760131+CamelotMacaw@users.noreply.github.com>
2024-12-01 18:10:31 +0100

    init

---
+++ Android/app/src/main/java/com/example/samplestickerapp/EditStickerActivity.java
@@ -0,0 +1,24 @@
+
+package com.example.samplestickerapp;
+
+import android.text.TextUtils;
+import android.util.JsonReader;
+
+import androidx.annotation.NonNull;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EditStickerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
