be50090972cb09e89647481e2ec143c791466ace
CamelotMacaw <91760131+CamelotMacaw@users.noreply.github.com>
2024-12-04 22:14:24 +0100

    comments

--- Android/app/src/main/java/com/example/samplestickerapp/EditStickerActivity.java
+++ Android/app/src/main/java/com/example/samplestickerapp/EditStickerActivity.java
@@ -2,39 +2,22 @@
 package com.example.samplestickerapp;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.provider.MediaStore;
-import android.text.TextUtils;
-import android.util.JsonReader;
 import android.os.Build;
-import androidx.annotation.NonNull;
-
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import android.app.Activity;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import android.content.Intent;
-import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-import android.os.Parcelable;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 
 
 public class EditStickerActivity extends AppCompatActivity {
@@ -53,18 +36,9 @@ public class EditStickerActivity extends AppCompatActivity {
         imageView = findViewById(R.id.image_of_edited_sticker);
         addImageBtn = findViewById(R.id.add_image_btn);
 
-//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-//                != PackageManager.PERMISSION_GRANTED) {
-//            ActivityCompat.requestPermissions(this,
-//                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-//                    REQUEST_PERMISSION_CODE);
-//        }
-
-//        addImageBtn.setOnClickListener(v -> selectImage());
-
         addImageBtn.setOnClickListener(v -> {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { // check if android version is high enough for permissions to even exist
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) // if permission for external storage hasn't been granted yet, ask for it
                         != PackageManager.PERMISSION_GRANTED) {
                     requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
                 } else {
@@ -80,41 +54,33 @@ public class EditStickerActivity extends AppCompatActivity {
     }
 
     private void openImagePicker() {
+        /**
+         * Opens Image Picker
+         */
+        // you're telling it that you want to pick something and the 2nd argument indicates the location; photos are in the externals storage
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // further specify type to get only the type of file you want
         intent.setType("image/*");
+        // tell it to start an activity to fulfill this intent. PICK_IMAGE_REQUEST is basically just an ID, it can be any number
+        // just makes it possible to identify the response that the activity will deliver
         startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
 
-//    private void selectImage() {
-//        // https://developer.android.com/guide/components/intents-common#GetFile
-//        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-//        intent.setType("image/*");
-//        if (intent.resolveActivity(getPackageManager()) != null) {
-//            startActivityForResult(intent, REQUEST_IMAGE_GET);
-//        }
-//    }
-
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if (requestCode == REQUEST_IMAGE_GET && resultCode == RESULT_OK) {
-//            Toast.makeText(EditStickerActivity.this, "Asked to add image!", Toast.LENGTH_SHORT).show();
-//            //Bitmap thumbnail = data.getParcelable("data", Bitmap.class);
-//            Uri fullPhotoUri = data.getData();
-//
-//        }
-//
-//    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+        // here you use PICK_IMAGE_REQUEST to identify the result you asked for
+        // you also check whether it was successful, whether there's actual data to process
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            // Android doesn't pass the full image, just the URI. You still have to load the image manually.
             Uri imageUri = data.getData();
             try {
                 Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
                 imageView.setImageBitmap(bitmap);
+
+                addImageBtn.setVisibility(View.GONE); // hide the button after successful upload
             } catch (Exception e) {
                 e.printStackTrace();
                 Toast.makeText(this, "Failed to load image", Toast.LENGTH_SHORT).show();
